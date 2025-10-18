24812456409538a0b29dd7a9f9faa1b06433f7fa
Lucas <luqueuris150@gmail.com>
2025-10-18 12:05:01 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 @echo off
-REM change 'python' to 'python3' or accordingly, depending on how python is called in your OS' variables
+REM change 'python' to 'python3' or accordingly, depending on how python is called in your system's variables.
+REM Change also "samble.bib" file to the name of the one that you want it to read.
+REM The "sample_clean.bib" can be modified to generate an output file with a custom name as well. 
 python bib_clean_and_map.py sample.bib sample_clean.bib
